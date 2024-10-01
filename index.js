d0cc25ff90cb46d9137205f2affc43a59d7169aa
Abdirabi440 <osmanabdirabi440@gmail.com>
2024-10-01 15:37:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+// NO. OF BLOCKS TO HEADQUARTERS
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+  
+// DISTANCE IN FEET
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+  
+// DISTANCE TRAVELED BETWEEN 2 BLOCKS (IN FEET)
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+  
+// FARE PRICE:
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {  // Corrected distance range
+        return (distance - 400) * 0.02;  // Charges 2 cents per foot after the first 400 feet
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far';
+    }
+}
